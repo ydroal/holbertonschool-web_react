5ef0b72e9ec56e344f5d9bc805a2ab44e4392121
ydroal <droal.yoko@gmail.com>
2024-01-06 17:31:09 +0000

    add: task7

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -59,5 +59,12 @@ export function executeWork(employee: DirectorInterface | TeacherInterface): str
   }
 }
 
-// console.log(executeWork(createEmployee(200)));
-// console.log(executeWork(createEmployee(1000)));
+export type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') return 'Teaching Math';
+  if (todayClass === 'History') return 'Teaching History';
+}
+
+// console.log(teachClass('Math'));
+// console.log(teachClass('History'));
