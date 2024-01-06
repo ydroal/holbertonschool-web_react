2ed13e84e009c4455ddd63dd93647e2fa8225997
ydroal <droal.yoko@gmail.com>
2024-01-06 14:42:26 +0000

    add: task4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,4 +19,37 @@ const printTeacher: printTeacherFunction = function(firstName: string, lastName:
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-// console.log(printTeacher("John", "Doe"));
+export interface StudentConstructor {
+  // コンストラクタのシグネチャ
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+export interface StudentClassInterface {
+  // メソッドのシグネチャ
+  displayName(): string;
+  workOnHomework(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// const student1 = new StudentClass('yoko', 'ONO');
+// console.log(student1.workOnHomework());
+// console.log(student1.displayName());
+
+
