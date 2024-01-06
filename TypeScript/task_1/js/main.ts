fc60d58d3480f3fc02af023480e3515bc5557b6a
ydroal <droal.yoko@gmail.com>
2024-01-06 14:10:28 +0000

    add: task3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,11 +11,12 @@ export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// const director1: Directors = {
-//   firstName: 'John',
-//   lastName: 'Doe',
-//   location: 'London',
-//   fullTimeEmployee: true,
-//   numberOfReports: 17,
-// };
-// console.log(director1);
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// console.log(printTeacher("John", "Doe"));
