16b94e69ce064a3095a08e3af962c6f5f862281c
ydroal <droal.yoko@gmail.com>
2024-01-07 01:12:17 +0000

    add: task10

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+export interface MajorCredits {
+  credits: number;
+  _majorCreditBrand: void;
+}
+
+export interface MinorCredits {
+  credits: number;
+  _minorCreditBrand: void;
+}
+
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits } as MajorCredits;
+}
+
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits } as MinorCredits;
+}
