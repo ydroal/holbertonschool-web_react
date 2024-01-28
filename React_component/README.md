de2e3b3e69cc9ce95ef2ab55682dd716c6b6f9cd
ydroal <droal.yoko@gmail.com>
2024-01-28 11:47:22 +0000

    add: task0

---
+++ React_component/README.md
@@ -0,0 +1,8 @@
+# React component
+
+## Requirements
+
+- All your files will be interpreted/compiled on Ubuntu 18.04 LTS using node 12.x.x and npm 6.x.x
+- Allowed editors: vi, vim, emacs, Visual Studio Code
+- All your files should end with a new line
+- A README.md file, at the root of the folder of the project, is mandatory
---
+++ React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import Login from '../Login/Login'
+import Notifications from '../Notifications/Notifications'
+import CourseList from '../CourseList/CourseList'
+import { getLatestNotification } from '../utils/utils';
+import './App.css';
+
+const listCourses = [
+  {
+    id: 1,
+    name: 'ES6', 
+    credit: 60
+  },
+  {
+    id: 2,
+    name: 'Webpack', 
+    credit: 20
+  },
+  {
+    id: 3,
+    name: 'React', 
+    credit: 40
+  },
+];
+
+const listNotifications = [
+  {
+    id: 1,
+    type: 'default',
+    value: 'New course available'
+  },
+  {
+    id: 2,
+    type: 'urgent',
+    value: 'New resume available'
+  },
+  {
+    id: 3,
+    type: 'urgent',
+    html: {__html: getLatestNotification()}
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="App">
+          <div className="Header">
+            <Header />
+            <Notifications listNotifications={listNotifications} />
+          </div>
+          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+}
+
+export default App;
