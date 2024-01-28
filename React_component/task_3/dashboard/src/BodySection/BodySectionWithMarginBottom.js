f31f30d8bfb18277e87eea901c6b97a1b813120e
ydroal <droal.yoko@gmail.com>
2024-01-28 15:53:18 +0000

    add: task4

---
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+
+class BodySectionWithMarginBottom extends Component {
+  render() {
+    const { title, children } = this.props;
+
+    return (
+      <div className="bodySectionWithMargin">
+        <BodySection title={title}>
+        {children}
+        </BodySection>
+      </div>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node
+};
+
+export default BodySectionWithMarginBottom;
